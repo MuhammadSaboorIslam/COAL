86fd2d80b74053f5ea7d3b877c8aaf7735ec81c9
Muhammad Saboor Islam <97667489+MuhammadSaboorIslam@users.noreply.github.com>
2024-10-16 00:05:27 +0500

    Create even or odd.asm

---
+++ even or odd.asm
@@ -0,0 +1,5 @@
+test ax, 1           ; Test if the number is odd (AX & 1)
+    jz skip              ; If even, skip it
+
+test ax, 1           ; Test if the number is odd (AX & 1)
+    jnz skip             ; If odd, skip it
